03eaaefbba8d1774b8ead78a925180f6cb9f30d7
Rasmusdamaau <rasmus72dam@gmail.com>
2021-02-23 15:40:20 +0100

    opg. 4

--- Opgaver_Orimar/Lektion_5/Sim_BM.R
+++ Opgaver_Orimar/Lektion_5/Sim_BM.R
@@ -4,14 +4,14 @@ sim_BM <- function(T_, delta) {
   tmesh <- seq(from = 0, to = T_, by = delta)
   N <- length(tmesh)
   step <- delta
-  Norm <- rnorm(N-1)
-  B <- c(0,cumsum(sqrt(step) * Norm))
-  return(B)
+  Norm <- rnorm(N)
+  B <- c(0,cumsum(sqrt(step) * Norm[-length(Norm)]))
+  output <- data.frame(B = B, Norm = Norm)
+  return(output)
   # plot(x = tmesh, y = B, type = "l")
 }
 
 
-set.seed(2)
 
 b <- function(t, X_t) {
   -X_t
@@ -24,13 +24,29 @@ sigma <- function(t, X_t) {
 Euler_scheme <- function(delta, T_ = 1, b, sigma, x_0 = 0) {
   N_n <- ceiling(T_ / delta)
   tmesh <- seq(from = 0, to = T_, by = delta)
-  B <- sim_BM(T_, delta)
+  sim_BM_result <- sim_BM(T_, delta)
+  B <- sim_BM_result$B
+  Norm <- sim_BM_result$Norm
   X <- c(x_0)
+  X_2 <- c(x_0)
+  step <- delta * T_
   for (k in 2:(N_n-1)) {
-    X[k] <- X[k-1] + b(k,X[k-1])*delta + sigma(k,X[k-1])*(B[k] - B[k-1])
+    X[k] <- X[k-1] + b(k,X[k-1])*step + sigma(k,X[k-1])*(B[k] - B[k-1])
+    X_2[k] <- exp(-step) * X_2[k-1] + sqrt(-1/2 * (1- exp(2*step))) * Norm[k]
   }
-  return(X)
+  output <- data.frame(sim_sol = X, real_sol = X_2)
+  return(output)
 }
 
+set.seed(2)
 
-Euler_scheme(delta = 1/1000, T_ = 1, b = b, sigma = sigma, x_0 = 0)
+result <- Euler_scheme(delta = 1/1000, T_ = 1, b = b, sigma = sigma, x_0 = 0)
+result
+
+library(tidyverse)
+
+ggplot(data = result, aes(x=seq_len(nrow(result)), y = sim_sol)) +
+  geom_line(color = "red") +
+  geom_line(aes(x=seq_len(nrow(result)), y = real_sol))
+
+# plot.ts(result)
